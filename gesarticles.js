342a134a169925980d421fac9e5c38c81f09cdd4
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2022-11-30 17:00:29 +0100

    gestion

--- gesarticles.js
+++ gesarticles.js
@@ -9,7 +9,7 @@ const mis1 = document.querySelector(".mis1");
 const mis2 = document.querySelector(".mis2");
 const button = document.querySelector(".btn");
 let arr = [];
-let ajt = document.getElementById("A"); 
+let ajt = document.getElementById("A");
 var promo;
 
 function validatenom(lnom) {
@@ -33,8 +33,13 @@ function validatemarque(lmarque) {
 function validateprix(lprix) {
   if (lprix.value.trim() === "") {
     setErrorFor(lprix, "price  is required");
-  } else if (lprix.value.length < 1 || lprix.value.length > 30 || lprix.value.match(/([A-Z]{3}|[A-Z]?[\$€¥])?\s?(\d{1,3}((,\d{1,3})+)?(.\d{1,3})?(.\d{1,3})?(,\d{1,3})?)/ )
-    ) {
+  } else if (
+    lprix.value.length < 1 ||
+    lprix.value.length > 30 ||
+    lprix.value.match(
+      /([A-Z]{3}|[A-Z]?[\$€¥])?\s?(\d{1,3}((,\d{1,3})+)?(.\d{1,3})?(.\d{1,3})?(,\d{1,3})?)/
+    )
+  ) {
     setSuccessFor(lprix, "Looks Good!");
   } else {
     setErrorFor(lprix, "price is invalid");
@@ -64,17 +69,22 @@ function boom() {
     setSuccessFor(nom, "Looks Good!");
     arr.push(true);
   }
-  console.log(arr.length)
+  console.log(arr.length);
   if (prixValue === "") {
     setErrorFor(prix, "price  is required");
-  } else if ((prixValue.length > 1) && prixValue.length < 30 && prixValue.match(/([A-Z]{3}|[A-Z]?[\$€¥])?\s?(\d{1,3}((,\d{1,3})+)?(.\d{1,3})?(.\d{1,3})?(,\d{1,3})?)/ )
-    ) {
+  } else if (
+    prixValue.length > 1 &&
+    prixValue.length < 30 &&
+    prixValue.match(
+      /([A-Z]{3}|[A-Z]?[\$€¥])?\s?(\d{1,3}((,\d{1,3})+)?(.\d{1,3})?(.\d{1,3})?(,\d{1,3})?)/
+    )
+  ) {
     setSuccessFor(prix, "Looks Good!");
     arr.push(true);
   } else {
     setErrorFor(prix, "price is invalid");
   }
-  console.log(arr.length)
+  console.log(arr.length);
   if (marqueValue === "") {
     setErrorFor(marque, "marque is required");
   } else if (marqueValue.length < 2 || marqueValue.length > 30) {
@@ -83,38 +93,38 @@ function boom() {
     setSuccessFor(marque, "Looks Good!");
     arr.push(true);
   }
-  console.log(arr.length)
+  console.log(arr.length);
   if (dateValue === "") {
     setErrorFor(date, "Date is required");
   } else {
     setSuccessFor(date, "Looks Good!");
     arr.push(true);
   }
-  console.log(arr.length)
+  console.log(arr.length);
   var gen = !Promo[0].checked && !Promo[1].checked;
 
   if (Promo[0].checked) {
     arr.push(true);
     mis1.innerHTML = "Look good !";
     mis1.style.color = "green";
-    promo=document.getElementById("o").value;
-  } else if(Promo[1].checked){
+    promo = document.getElementById("o").value;
+  } else if (Promo[1].checked) {
     arr.push(true);
     mis1.innerHTML = "Look good !";
     mis1.style.color = "green";
-    promo=document.getElementById("n").value;
-  }else{
+    promo = document.getElementById("n").value;
+  } else {
     mis1.innerHTML = "Choose one";
     mis1.style.color = "red";
   }
-  console.log(arr.length)
-if (typeValue ===''){
-  setErrorFor(type,"choose one");
-}else{
-  setSuccessFor(type,"Looks good!");
-  arr.push(true);
-}
-console.log(arr.length)
+  console.log(arr.length);
+  if (typeValue === "") {
+    setErrorFor(type, "choose one");
+  } else {
+    setSuccessFor(type, "Looks good!");
+    arr.push(true);
+  }
+  console.log(arr.length);
 }
 function setErrorFor(input, message) {
   const formControl = input.parentElement;
@@ -131,17 +141,31 @@ function setSuccessFor(input, message) {
 
 var selectedRow = null;
 ajt.onclick = function getitdone() {
-  arr.length = 0;
-  boom()
-  if(arr.length!=6){
+  if (ajt.value === "Ajouter") {
     arr.length = 0;
     boom();
-  }else {
-    var formData = readFormData();
-    insertNewRecord(formData);
-    resetform();
+    if (arr.length != 6) {
+      arr.length = 0;
+      boom();
+    } else {
+      var formData = readFormData();
+      insertNewRecord(formData);
+      resetform();
+    }
+  } else if (ajt.value === "Modifier") {
+    arr.length = 0;
+    boom();
+    if (arr.length != 6) {
+      arr.length = 0;
+      boom();
+    } else {
+      var formData = readFormData();
+      updateRecord(formData);
+      resetform();
+      document.getElementById("A").value = "Ajouter";
+    }
   }
-}
+};
 // function getitdone(){
 //   arr.length = 0;
 //   boom()
@@ -152,11 +176,8 @@ ajt.onclick = function getitdone() {
 //   }
 // }
 
-function onFormSubmit() {
- 
-}
 function readFormData() {
-  var formData =[];
+  var formData = [];
   formData["nom"] = document.getElementById("nom").value;
   formData["prix"] = document.getElementById("prix").value;
   formData["marque"] = document.getElementById("marque").value;
@@ -171,10 +192,10 @@ function insertNewRecord(data) {
     .getElementById("productsdetails")
     .getElementsByTagName("tbody")[0];
   var newRow = table.insertRow(table.length);
-//   for (let i=0;i<table.length;i++) {
-//   var x=newRow.insertElement(i);
-//       x.innerHTML= data.table[i];
-// }
+  //   for (let i=0;i<table.length;i++) {
+  //   var x=newRow.insertElement(i);
+  //       x.innerHTML= data.table[i];
+  // }
   cell1 = newRow.insertCell(0);
   cell1.innerHTML = data.nom;
   cell2 = newRow.insertCell(1);
@@ -188,50 +209,40 @@ function insertNewRecord(data) {
   cell6 = newRow.insertCell(5);
   cell6.innerHTML = data.promo;
   cell6 = newRow.insertCell(6);
-  cell6.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                     <a onClick="onDelete(this)">Delete</a>`;
+  cell6.innerHTML = `<button onClick="onEdit(this)">Edit</button>
+  <button onclick="onDelete(this)">Delete</button>`;
 }
-function resetform(){
-document.getElementById("nom").value="";
-document.getElementById("prix").value="";
-document.getElementById("marque").value="";
-document.getElementById("date").value="";
-document.getElementById("type").value="";
-Promo[0].checked=Promo[0].unchecked;
-Promo[1].checked=Promo[1].unchecked;
-var selectedRow = null;
-}
-function onEdit(td){
-  document.getElementById("A").value = "modifier";
-  document.getElementById("A").id = "M";
-  let mdf = document.getElementById("M"); 
-selectedRow =td.parentElement.parentElement;
-document.getElementById("nom").value= selectedRow.cells[0].innerHTML;
-document.getElementById("prix").value= selectedRow.cells[1].innerHTML;
-document.getElementById("marque").value= selectedRow.cells[2].innerHTML;
-document.getElementById("date").value= selectedRow.cells[3].innerHTML;
-document.getElementById("type").value= selectedRow.cells[4].innerHTML;
-if (selectedRow.cells[5].innerHTML === "Oui") {
-  document.getElementById("o").checked = true;
-} else  {
-  document.getElementById("n").checked = true;
-}
-mdf.onclick = function () {
-  arr.length = 0;
-  boom();
-  if (arr.length != 6) {
-    arr.length = 0;
-    boom();
-  } else {
-    var formData = readFormData();
-    updateRecord(formData);
-        resetform();
-    document.getElementById("M").value = "Ajouter";
-    document.getElementById("M").id = "A";
+function resetform() {
+  document.getElementById("nom").value = "";
+  document.getElementById("prix").value = "";
+  document.getElementById("marque").value = "";
+  document.getElementById("date").value = "";
+  document.getElementById("type").value = "";
+  Promo[0].checked = Promo[0].unchecked;
+  Promo[1].checked = Promo[1].unchecked;
+  var selectedRow = null;
+  const ff = document.querySelectorAll(".form-control");
+  for (let i = 0; i < ff.length; i++) {
+    if (ff[i].classList.contains("success")) {
+      ff[i].classList.remove("success");
+    }
   }
-};
 }
-function updateRecord(formData){
+function onEdit(td) {
+  document.getElementById("A").value = "Modifier";
+  selectedRow = td.parentElement.parentElement;
+  document.getElementById("nom").value = selectedRow.cells[0].innerHTML;
+  document.getElementById("prix").value = selectedRow.cells[1].innerHTML;
+  document.getElementById("marque").value = selectedRow.cells[2].innerHTML;
+  document.getElementById("date").value = selectedRow.cells[3].innerHTML;
+  document.getElementById("type").value = selectedRow.cells[4].innerHTML;
+  if (selectedRow.cells[5].innerHTML === "Oui") {
+    document.getElementById("o").checked = true;
+  } else {
+    document.getElementById("n").checked = true;
+  }
+}
+function updateRecord(formData) {
   selectedRow.cells[0].innerHTML = formData.nom;
   selectedRow.cells[1].innerHTML = formData.prix;
   selectedRow.cells[2].innerHTML = formData.marque;
@@ -239,16 +250,30 @@ function updateRecord(formData){
   selectedRow.cells[4].innerHTML = formData.type;
   selectedRow.cells[5].innerHTML = formData.promo;
   if (Promo[0].checked) {
-     selectedRow.cells[5].innerHTML = document.getElementById("o").value;
+    selectedRow.cells[5].innerHTML = document.getElementById("o").value;
   } else {
-     selectedRow.cells[5].innerHTML = document.getElementById("n").value;
-  }
- 
-}
-function onDelete(td){
-  if(confirm('are you sure to delete this record?')){
-   row = td.parentElement.parentElement;
-   document.getElementById("productsdetails").deleteRow(row.rowIndex);
-   resetform();
+    selectedRow.cells[5].innerHTML = document.getElementById("n").value;
   }
 }
+let getrow
+function onDelete(td) {
+  getrow=td.parentElement.parentElement;
+  document.getElementById('id01').style.display='block'
+  // if(confirm('are you sure to delete this record?')){
+
+  // }
+}
+function delet()
+{
+  document.getElementById("productsdetails").deleteRow(getrow.rowIndex);
+  document.getElementById('id01').style.display='none'
+
+}
+// Get the modal
+var modal = document.getElementById("id01");
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
